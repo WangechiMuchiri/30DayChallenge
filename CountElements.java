5e91c74841751e8b55d993a5fd71871ca6148bef
WangechiMuchiri <49262911+WangechiMuchiri@users.noreply.github.com>
2020-05-05 15:35:47 +0300

    Add files via upload

---
+++ CountElements.java
@@ -0,0 +1,40 @@
+
+import java.util.HashMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Wangechi
+ */
+public class CountElements {
+    public static int countElements(int [] arr){
+        HashMap <Integer, Integer> myMap = new HashMap<Integer,Integer>();
+        int counter = 0,count = 0;
+        for(int i = 0; i<arr.length;i++){
+            if(myMap.containsKey(arr[i]-1) || myMap.containsKey(arr[i]+1)){
+              myMap.put(arr[i],counter);
+              count++;
+              
+                
+            }
+            else{
+                myMap.put(arr[i],counter);
+                
+            }
+            
+        }
+        return count;
+        
+        
+    }
+    public static void main(String[] args) {
+        int [] myArray = {1,2,3};
+        countElements(myArray);
+    }
+    
+}
